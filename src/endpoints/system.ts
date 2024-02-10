a7da7158b961ef9159a9029993b4983c1c2b14d9
Kirill Zhirnov <ekvixgroup@gmail.com>
2024-02-10 18:39:51 +0100

    minor fix

--- src/endpoints/system.ts
+++ src/endpoints/system.ts
@@ -1,12 +1,15 @@
 import {BoundlessClient} from '../client';
 import {createGetStr} from '../utils';
+import {TAdapterExtraRequestInit} from '../axiosFetchAdapter';
 
 export default class SystemApi {
 	constructor(protected client: BoundlessClient) {
 	}
 
-	async fetchSettings(keys: string[]): Promise<{[key: string]: any}> {
-		const {data} = await this.client.createRequest().get(`/system/settings/fetch?${createGetStr({keys})}`);
+	async fetchSettings(keys: string[], extraRequest: TAdapterExtraRequestInit = {}): Promise<{[key: string]: any}> {
+		const {data} = await this.client.createRequest({extraRequest})
+			.get(`/system/settings/fetch?${createGetStr({keys})}`)
+		;
 		return data;
 	}
 }
